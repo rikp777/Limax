266ecfe340463d9b836f60036920db9ab379e504
rikp777 <rikpeeter11@hotmail.com>
2019-08-16 11:33:30 +0200

    Final fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,5 @@
     </body>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/manifest.js') }}" defer></script>
-    <script src="{{ asset('js/vendor.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 </html>
