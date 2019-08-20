1a54b7f360cf1ed27226b079ca44f94547a09bbb
rikp777 <rikpeeter11@hotmail.com>
2019-08-20 12:02:46 +0200

    footer

--- resources/js/components/partials/AppFooter.vue
+++ resources/js/components/partials/AppFooter.vue
@@ -2,8 +2,9 @@
   <section class="app-footer">
     <footer class="footer">
       <div class="container-fluid clearfix">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018 <a href="http://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="mdi mdi-heart text-danger"></i></span>
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2019 <a href="http://www.limax.nl" target="_blank">Limax</a>. All rights reserved.</span>
+          <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Made by <a href="http://www.linkedin.com/in/rikpeeters-nl" target="_blank">Rik Peeters</a>.</span>
+        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <font-awesome-icon icon="heart"></font-awesome-icon></span>
       </div>
     </footer>
   </section>
