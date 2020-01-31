55b097af98beb00c544d52fd5e6c1227c86c697c
Tommvv <tomvvegchel09@gmail.com>
2020-01-31 16:03:08 +0100

    setup cmd

--- Setup.bat
+++ Setup.bat
@@ -1,58 +1,104 @@
 @ECHO OFF
-ECHO =========================
-ECHO . "_  ._"
-ECHO . "| |(_)                                     _"
-ECHO . "| | _  ____    ____  _   _     ___   ____ | |_   _   _  ____"
-ECHO . "| || ||    \  / _  |( \ / )   /___) / _  )|  _) | | | ||  _ \"
-ECHO . "| || || | | |( ( | | ) X (   |___ |( (/ / | |__ | |_| || | | |"
-ECHO . "|_||_||_|_|_| \_||_|(_/ \_)  (___/  \____) \___) \____|| ||_/"
-ECHO . "                                                       |_|"
+Title Limax FlowControl Setup
+mode con cols=64 lines=35 & color 0B
+REM You can change the variable PeriodTime as you needs here is set to (120 seconds = 2 minutes) as example
+Set "PeriodTime=2"
+echo(
+cls
+:::
+:::  _      _                         _____      _
+::: | |    (_)                       / ____|    | |
+::: | |     _ _ __ ___   __ ___  __ | (___   ___| |_ _   _ _ __
+::: | |    | | '_ ` _ \ / _` \ \/ /  \___ \ / _ | __| | | | '_ \
+::: | |____| | | | | | | (_| |>  <   ____) |  __| |_| |_| | |_) |
+::: |______|_|_| |_| |_|\__,_/_/\_\ |_____/ \___|\__|\__,_| .__/
+:::                                                       | |
+:::                                                       |_|
+:::
+:::              _ _,---._
+:::           ,-','       `-.___
+:::          /-;'               `._
+:::         /\/          ._   _,'o \
+:::        ( /\       _,--'\,','"`. )
+:::         |\      ,'o     \'    //\
+:::         |      \        /   ,--'""`-.
+:::         :       \_    _/ ,-'         `-._
+:::          \        `--'  /                )
+:::           `.  \`._    ,'     ________,','
+:::             .--`     ,'  ,--` __\___,;'
+:::              \`.,-- ,' ,`_)--'  /`.,'
+:::               \( ;  | | )      (`-/
+:::                 `--'| |)       |-/
+:::                   | | |        | |
+:::                   | | |,.,-.   | |_
+:::                   | `./ /   )---`  )
+:::                  _|  /    ,',   ,-'
+:::                 ,'|_(    /-<._,' |--,
+:::                 |    `--'---.     \/ \
+:::                 |          / \    /\  \
+:::               ,-^---._     |  \  /  \  \
+:::            ,-'        \----'   \/    \--`.
+:::           /            \              \   \
+:::
 
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 
-ECHO =========================
-ECHO ======setup Composer========
-ECHO =========================
-call composer install
+>NUL TIMEOUT /T %PeriodTime% /NOBREAK
 
+REM
+REM DEMO - how to launch several processes in parallel, and wait until all of them finish.
+REM
 
-ECHO =========================
-ECHO ========key:generate=========
-ECHO =========================
-call php artisan key:generate
+@ECHO OFF
+start "!ComposerInstall!" call composer install
+:waittofinishComposerInstall
+echo Composer Install is still running...
+timeout /T 2 /nobreak >nul
+tasklist.exe /fi "WINDOWTITLE eq !ComposerInstall!" | find ":" >nul
+if errorlevel 1 goto waittofinishComposerInstall
+start "!KeyGenerate!" call php artisan key:generate
 
+:waittofinishKeyGenerate
+echo Key Generate is still running...
+timeout /T 2 /nobreak >nul
+tasklist.exe /fi "WINDOWTITLE eq !KeyGenerate!" | find ":" >nul
+if errorlevel 1 goto waittofinishKeyGenerate
+start "!StorageLink!" call php artisan storage:link
 
-ECHO =========================
-ECHO ========storage:link=========
-ECHO =========================
-call php artisan storage:link
+:waittofinishStorageLink
+echo Storage Link is still running...
+timeout /T 2 /nobreak >nul
+tasklist.exe /fi "WINDOWTITLE eq !StorageLink!" | find ":" >nul
+if errorlevel 1 goto waittofinishStorageLink
+start "!NpmInstall!" call npm install
 
+:waittofinishRunDev
+echo Npm Install is still running...
+timeout /T 2 /nobreak >nul
+tasklist.exe /fi "WINDOWTITLE eq !NpmInstall!" | find ":" >nul
+if errorlevel 1 goto waittofinishRunDev
+start "!RunDev!" call npm run dev
 
-ECHO =========================
-ECHO ========npm install=========
-ECHO =========================
-call npm install
+:waittofinishMigrateFreshSeed
+echo Run Dev is still running...
+timeout /T 2 /nobreak >nul
+tasklist.exe /fi "WINDOWTITLE eq !RunDev!" | find ":" >nul
+if errorlevel 1 goto waittofinishMigrateFreshSeed
+start "!MigrateFreshSeed!" call php artisan migrate:fresh --seed
 
+:waittofinishPassportInstall
+echo Migrate Fresh Seed is still running...
+timeout /T 2 /nobreak >nul
+tasklist.exe /fi "WINDOWTITLE eq !MigrateFreshSeed!" | find ":" >nul
+if errorlevel 1 goto waittofinishPassportInstall
+start "!PassportInstall!" call php artisan passport:install
 
-ECHO =========================
-ECHO ========npm run dev========
-ECHO =========================
-call npm run dev
+:waittofinishDumpAutoload
+echo PassportInstall is still running...
+timeout /T 2 /nobreak >nul
+tasklist.exe /fi "WINDOWTITLE eq !PassportInstall!" | find ":" >nul
+if errorlevel 1 goto waittofinishDumpAutoload
+start "!DumpAutoload!" call composer dump-autoload
 
-
-ECHO =========================
-ECHO =====migrate:fresh --seed======
-ECHO =========================
-call php artisan migrate:fresh --seed
-
-
-ECHO =========================
-ECHO ======passport:install=========
-ECHO =========================
-call php artisan passport:install
-
-
-ECHO =========================
-ECHO ======dump-autoload=========
-ECHO =========================
-call composer dump-autoload
 PAUSE
+
