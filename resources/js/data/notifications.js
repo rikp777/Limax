3b9de06a793c1481afaa9220b660f92556c506b2
Tommvv <tomvvegchel09@gmail.com>
2020-03-12 16:14:06 +0100

    notification english

--- resources/js/data/notifications.js
+++ resources/js/data/notifications.js
@@ -1,9 +1,15 @@
 const data = [
-  {
-    img: '',
-    title: 'FlowControl heeft een nieuw uiterlijk, dit veranderd niks aan jullie manier van werken!',
-    date: '18.02.2020 - 12:45',
-    id: 1
-  }
+    {
+        img: '',
+        title: 'FlowControl heeft een nieuw uiterlijk, dit veranderd niks aan jullie manier van werken!',
+        date: '12.03.2020 - 12:45',
+        id: 1
+    },
+    {
+        img: '',
+        title: 'FlowControl has a new look, this does not change your way of working!',
+        date: '12.03.2020 - 12:45',
+        id: 2
+    }
 ]
 export default data
